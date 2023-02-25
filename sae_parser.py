937e21f409dacc54eed3a838200c810117b951dc
Cyril Vincent <contact@cyrilvincent.com>
2023-02-25 19:28:02 +0100

    generic

--- sae_parser.py
+++ sae_parser.py
@@ -1,46 +1,14 @@
 import art
 import config
-import pandas
 import argparse
-import os
+from generic_finess_parser import GenericFinessParser
 from sqlentities import Context
 
 
-class SAEParser:
+class SAEParser(GenericFinessParser):
 
     def __init__(self, context):
-        self.context = context
-
-    def scan(self, path: str):
-        print(f"Scan {path}")
-        l = os.listdir(path)
-        for f in l:
-            if f.endswith(".csv"):
-                self.load(path, f)
-
-    def get_name_from_file(self, file: str) -> str:
-        index = file.rindex("_")
-        s = file[0:index]
-        return s.lower()
-
-    def read_csv(self, path: str) -> pandas.DataFrame:
-        dtype = {"nofinesset": str, "nofinessj": str}
-        try:
-            df = pandas.read_csv(path, sep=";", dtype=dtype, low_memory=False)
-        except UnicodeDecodeError:
-            df = pandas.read_csv(path, sep=";", dtype=dtype, encoding="latin_1", low_memory=False)
-        return df
-
-    def load(self, path: str, file: str):
-        path = f"{path}/{file}"
-        print(f"Load {path}")
-        name = self.get_name_from_file(file)
-        df = self.read_csv(path)
-        df.columns= df.columns.str.strip().str.lower()
-        # df = df.drop(columns=["bor"])
-        context.create_engine()
-        with context.engine.begin() as connection:
-            df.to_sql(name, connection, schema="sae", if_exists="replace", index_label='id')
+        super().__init__(context)
 
 if __name__ == '__main__':
     art.tprint(config.name, "big")
@@ -58,7 +26,7 @@ if __name__ == '__main__':
     db_size = context.db_size()
     print(f"Database {context.db_name}: {db_size:.0f} Mb")
     sp = SAEParser(context)
-    sp.scan(args.path)
+    sp.scan(args.path, "sae")
     new_db_size = context.db_size()
     print(f"Database {context.db_name}: {new_db_size:.0f} Mb")
     print(f"Database grows: {new_db_size - db_size:.0f} Mb ({((new_db_size - db_size) / db_size) * 100:.1f}%)")
