199e75c7ab7691ca6605dd9526d5dd95201416a9
Cyril Vincent <contact@cyrilvincent.com>
2023-03-29 10:37:55 +0200

    views

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 name = "ICIP"
-version = "3.0.2.RC"
+version = "3.0.3.Release"
 copyright = "(c) Skema 2021 - 2023"
 adresse_path = "data/adresse"
 connection_string = "postgresql://postgres:sa@localhost/icip"
