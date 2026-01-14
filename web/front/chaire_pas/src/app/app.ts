d204cad5da1a64b7184c2a3f38193abe14363c14
Cyril Vincent <contact@cyrilvincent.com>
2026-01-14 20:08:00 +0100

    banner

--- web/front/chaire_pas/src/app/app.ts
+++ web/front/chaire_pas/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from "../shared/menu/menu";
 import { Banner } from '../shared/banner/banner';
+import { Footer } from "../shared/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, Banner],
+  imports: [RouterOutlet, Menu, Banner, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- web/front/chaire_pas/src/shared/banner/banner.ts
+++ web/front/chaire_pas/src/shared/banner/banner.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -8,4 +8,15 @@ import { Component, input } from '@angular/core';
 })
 export class Banner {
   activeMenu = input<number>(0);
+  activeImage = computed(() => `background-image: url('/img/banner${this.activeMenu()}.jpg');background-position: 50.00% ${this.activePosition()}%;`)
+  texts = ["Les missions de la Chaire", 
+    "L'observatoire de l'accès aux soins",
+    "Nos projets scientifiques",
+    "Open Data",
+    "On parle de nous",
+    "Soutenir la Chaire",
+  ];
+  activeText = computed(() => this.texts[this.activeMenu()]);
+  positions = [78.11, 50, 46.44, 83.89, 50, 65.6];
+  activePosition = computed(() => this.positions[this.activeMenu()]);
 }
