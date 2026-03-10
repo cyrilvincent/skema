5fe9b42b46e4e2bcd30a4e62b037c82303a2257e
Cyril Vincent <contact@cyrilvincent.com>
2026-03-10 13:16:52 +0100

    missions

--- web/front/chaire_pas/src/pages/home/home.ts
+++ web/front/chaire_pas/src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CommonService } from '../../shared/common.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
