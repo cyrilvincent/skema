80d3c4b937354a7ea89cd986ec65d621acea9c8b
Cyril Vincent <contact@cyrilvincent.com>
2026-05-21 20:54:38 +0200

    www.chaire_paas

--- web/back/start_gunicorn_stdout.sh
+++ web/back/start_gunicorn_stdout.sh
@@ -2,5 +2,4 @@
 source /home/cyrilvincent/venv/bin/activate
 gunicorn api:app -k uvicorn.workers.UvicornWorker --bind unix:/tmp/gunicorn.sock --workers 5 --timeout 300 --keep-alive 5 --log-level info
 
-# ps aux | grep gunicorn
-# kill -2 pid
+# pkill gunicorn
