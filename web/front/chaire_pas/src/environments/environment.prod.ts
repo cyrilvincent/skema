b76d1e4c1b90086a9ff94b93a1dde8fe187f98e6
Cyril Vincent <contact@cyrilvincent.com>
2026-05-21 10:06:55 +0200

    ngbuild prod

--- web/front/chaire_pas/src/environments/environment.prod.ts
+++ web/front/chaire_pas/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   version: "0.1.0.Beta",
   copyright: "SKEMA Business School & Université Côte d'Azur 2026",
-  baseUrl: "http://chaire-paas.dev.skema.edu/api", // TODO put httpS  // Ou mettre 82.97.33.133
+  baseUrl: "http://chaire-paas.dev.skema.edu/api", // TODO put httpS
   loading: "En cours de chargement",
   dns: "chaire-paas.com",
   accessibilite: 83,
