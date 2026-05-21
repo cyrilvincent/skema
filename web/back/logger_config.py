b79245e24c7dffe3f071a2eae82534bbc1d5b8e2
Cyril Vincent <contact@cyrilvincent.com>
2026-05-21 17:20:49 +0200

    logs

--- web/back/logger_config.py
+++ web/back/logger_config.py
@@ -5,42 +5,26 @@ import os
 
 
 def config(stdout=False):
-    env = os.environ.get("CHAIRE_PAAS", "dev")
+    env = os.environ['CHAIRE_PAAS'] if "CHAIRE_PAAS" in os.environ else "dev"
+    print(f"CHAIRE_PAAS {env} on {sys.platform}")
     is_prod = env == "prod"
-
-    root = logging.getLogger()
-    root.setLevel(logging.INFO)
-    root.handlers.clear()
-    root.propagate = False
-
-    formatter = logging.Formatter(
-        "%(asctime)s %(levelname)s %(name)s: %(message)s",
-        datefmt='%y-%m-%d %H:%M:%S'
-    )
-
-    if not stdout and is_prod:
-        handler = TimedRotatingFileHandler(
-            filename="logs/app.log",
-            when="midnight",
-            interval=1,
-            backupCount=10,
-            encoding="utf-8",
-        )
-        handler.setFormatter(formatter)
-        root.addHandler(handler)
-    else:
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-        root.addHandler(stream_handler)
-
-    for name in [
-        "uvicorn", "uvicorn.error", "uvicorn.access",
-        "gunicorn", "gunicorn.error", "gunicorn.access"
-    ]:
-        logger = logging.getLogger(name)
-        logger.handlers.clear()
-        logger.propagate = False
-
+    print(f"Create logging config prod=={is_prod}")
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+                        datefmt='%y-%m-%d %H:%M:%S')
+    # if not stdout and is_prod:
+    #     print("TimedRotatingFileHandler")
+    #     handler = TimedRotatingFileHandler(
+    #         filename="logs/app.log",
+    #         when="midnight",
+    #         interval=1,  # toutes les 24h
+    #         backupCount=10,
+    #         encoding="utf-8",
+    #     )
+    #     handler.suffix = "%Y-%m-%d"
+    #     logging.getLogger().addHandler(handler)
+    #     # logging.getLogger("uvicorn.error").addHandler(handler)
+    #     # logging.getLogger("uvicorn.access").addHandler(handler)
 
 
 
--- web/back/start_gunicorn.sh
+++ web/back/start_gunicorn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /home/cyrilvincent/venv/bin/activate
-gunicorn api:app -k uvicorn.workers.UvicornWorker --bind unix:/tmp/gunicorn.sock --workers 5 --timeout 300 --keep-alive 5 --log-level info --error-logfile logs/gunicorn-error.log --access-logfile logs/gunicorn-access.log
+gunicorn api:app -k uvicorn.workers.UvicornWorker --bind unix:/tmp/gunicorn.sock --workers 5 --timeout 300 --keep-alive 5 --log-level info --log-config logging.ini
+#--error-logfile logs/gunicorn-error.log --access-logfile logs/gunicorn-access.log
 
 # gunicorn api:app -k uvicorn.workers.UvicornWorker --bind 127.0.0.1:8000 --workers 5 --timeout 300 --keep-alive 5 --log-level info
 
