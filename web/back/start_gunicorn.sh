9de3be9f334868a9d633c82f575aac541dcaae4b
Cyril Vincent <contact@cyrilvincent.com>
2026-05-21 16:29:12 +0200

    upstream

--- web/back/start_gunicorn.sh
+++ web/back/start_gunicorn.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source /home/cyrilvincent/venv/bin/activate
 gunicorn api:app -k uvicorn.workers.UvicornWorker --bind 127.0.0.1:8000 --workers 5 --timeout 300 --keep-alive 5 --log-level info
+
+# ps aux | grep gunicorn
+# kill -2 pid
