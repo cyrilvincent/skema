d836ef7d6f06f457b8efe2b8506a5bc58d977228
Cyril Vincent <contact@cyrilvincent.com>
2026-05-22 07:37:34 +0200

    gunicorn back to http

--- web/back/start_gunicorn.sh
+++ web/back/start_gunicorn.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /home/cyrilvincent/venv/bin/activate
-gunicorn api:app -k uvicorn.workers.UvicornWorker --bind unix:/tmp/gunicorn.sock --workers 5 --timeout 300 --keep-alive 5 --log-level info --log-config logging.ini
+gunicorn api:app -k uvicorn.workers.UvicornWorker --bind 127.0.0.1:8000 --workers 1 --timeout 300 --keep-alive 5 --log-level info --log-config logging.ini
+
+# gunicorn api:app -k uvicorn.workers.UvicornWorker --bind unix:/tmp/gunicorn.sock --workers 5 --timeout 300 --keep-alive 5 --log-level info --log-config logging.ini
 #--error-logfile logs/gunicorn-error.log --access-logfile logs/gunicorn-access.log
 
 # gunicorn api:app -k uvicorn.workers.UvicornWorker --bind 127.0.0.1:8000 --workers 5 --timeout 300 --keep-alive 5 --log-level info
