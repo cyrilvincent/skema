3a65edcab8b0bab06be5362fb1aee01f729de102
Cyril Vincent <contact@cyrilvincent.com>
2026-05-26 15:24:22 +0200

    reditect apl => /

--- web/back/apl_service.py
+++ web/back/apl_service.py
@@ -591,8 +591,11 @@ if __name__ == '__main__':
     # s = json.dumps(export)
     # print(s[:5000])
     s.no_pickle = True
-    export = s.compute_iris("CC-02302", 10, 30, "HC", -0.12, "HD",
-                            with_sal=True)  # CC-38185 CC-38205 CC-38021 Autrans CC-38225 Autrans Meaudre CC-75101 CC-75056 CC-06088 CC-75101 CD-38 CD-06 CR-84 CR-93 CE-200040715 CA-381 CF-00
+    # export = s.compute_iris("CC-02302", 10, 30, "HC", -0.12, "HD",
+    #                         with_sal=True)  # CC-38185 CC-38205 CC-38021 Autrans CC-38225 Autrans Meaudre CC-75101 CC-75056 CC-06088 CC-75101 CD-38 CD-06 CR-84 CR-93 CE-200040715 CA-381 CF-00
+    # s = json.dumps(export)
+    # print(s[:5000])
+    export = s.compute_commune("CD-06", 10, 30, "HC", -0.12, "HD", with_sal=False)
     s = json.dumps(export)
     print(s[:5000])
 
